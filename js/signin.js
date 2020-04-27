a04476e2bab7bf55b6d0dd254a0f2fc58e89203a
chargeAhead <pranjalprateekofficial7@gmail.com>
2020-04-27 19:07:32 +0530

    update

--- js/signin.js
+++ js/signin.js
@@ -2,9 +2,30 @@ function msg()
 {
     var userid=document.getElementById("userid");
     var pas=document.getElementById("pass");
-    if(userid.value=="" || pas.value=="")
+    if(userid.value.trim()=="")
     {
-        alert("this feild connot be left blank");
+        alert("username connot be left blank");
+        userid.style.border ="solid 2px red";
     }
+    else if(pas.value.trim()=="")
+    {
+        alert("password connot be left blank");
+        pas.style.border ="solid 2px red";
+    }
+    else if(userid.value.trim().length<7)
+    {
+        alert("incorrect username")
+    }
+    else if(pas.value.trim().length<5)
+    {
+        alert("incorrect password")
+    }
+    else if(userid.value.trim().length>=7||pas.value.trim().length>=5)
+    {
+        alert("login sucessfull")
+    }
+    else
+    {
     alert("are you sure you want to submit");
+    }
 }
